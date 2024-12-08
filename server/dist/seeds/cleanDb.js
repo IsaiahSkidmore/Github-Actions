03c06d7a89f4e77264be7878352bbbdf4e8c536e
Isaiah Skidmore <isaiahschardt0617@gmail.com>
2024-12-08 13:11:14 -0800

    hopefully this fixes the poorly written code the bootcamp gave me

--- server/dist/seeds/cleanDb.js
+++ server/dist/seeds/cleanDb.js
@@ -1,7 +1,5 @@
-
 // import models from '../models/index.js';
 // import db from '../config/connection.js';
-
 // export default async (modelName: "Question", collectionName: string) => {
 //   try {
 //     let modelExists = [];
@@ -12,35 +10,29 @@
 //         }).toArray();
 //       }
 //     }
-
 //     if (modelExists.length) {
 //       await db.dropCollection(collectionName);
 //     }
-
 //   } catch (err) {
 //     throw err;
 //   }
 // }
-
 import models from '../models/index.js';
 import db from '../config/connection.js';
-
-export default async (modelName: "Question", collectionName: string) => {
-  try {
-    let modelExists = [];
-    const modelDb = models[modelName]?.db?.db;
-
-    if (modelDb) {
-      modelExists = await modelDb.listCollections({
-        name: collectionName
-      }).toArray();
+export default async (modelName, collectionName) => {
+    try {
+        let modelExists = [];
+        const modelDb = models[modelName]?.db?.db;
+        if (modelDb) {
+            modelExists = await modelDb.listCollections({
+                name: collectionName
+            }).toArray();
+        }
+        if (modelExists.length) {
+            await db.dropCollection(collectionName);
+        }
     }
-
-    if (modelExists.length) {
-      await db.dropCollection(collectionName);
+    catch (err) {
+        throw err;
     }
-
-  } catch (err) {
-    throw err;
-  }
-}
+};
